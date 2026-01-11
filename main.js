f3dd6d2fa4166bb3df1dd42d4a716c702e1f3c93
Bui Tan Phat <phatbtts02863@gmail.com>
2026-01-11 15:51:45 +0700

    save

---
+++ main.js
@@ -0,0 +1,10 @@
+// slider
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 3000);
+//end slider
